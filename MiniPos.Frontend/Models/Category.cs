4c1563996351f09346c4ad49c8b3b2e7505d213e
Kaung Min Htet <kgminhtet.dev@gmail.com>
2026-03-24 20:23:42 +0630

    fix/frontend(Cetegory): Add category details and create category page

--- MiniPos.Frontend/Models/Category.cs
+++ MiniPos.Frontend/Models/Category.cs
@@ -3,7 +3,8 @@ namespace MiniPos.Frontend.Models;
 public class Category
 {
     public Guid Id { get; set; }
-    public Guid MerchantId { get; set; }
+    public Merchant? Merchant { get; set; }
     public string Name { get; set; } = null!;
     public string? Description { get; set; }
+    public DateTime CreatedAt { get; set; }
 }
