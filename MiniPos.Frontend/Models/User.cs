3c417db2cc53dc600794d24e7b54c41b4ee75c2d
Kaung Min Htet <kgminhtet.dev@gmail.com>
2026-03-25 09:31:04 +0630

    fix/frontend(User): Fix and redesign

--- MiniPos.Frontend/Models/User.cs
+++ MiniPos.Frontend/Models/User.cs
@@ -3,8 +3,8 @@ namespace MiniPos.Frontend.Models;
 public class User
 {
     public Guid Id { get; set; }
-    public Guid? MerchantId { get; set; }
-    public Guid? BranchId { get; set; }
+    public Merchant? Merchant { get; set; }
+    public Branch? Branch { get; set; }
     public string UserName { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string Role { get; set; } = null!;
