fb85f37214a461423df3fd8c2c0b6b966abbb673
Kaung Min Htet <kgminhtet.dev@gmail.com>
2026-04-08 15:13:53 +0630

    fix: fix calling refresh token

--- MiniPos.Frontend/Program.cs
+++ MiniPos.Frontend/Program.cs
@@ -9,13 +9,13 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddTransient<CookiesHandler>();
-builder.Services.AddTransient<UnauthorizedHandler>();
+builder.Services.AddTransient<RefreshTokenHandler>();
 builder.Services.AddSingleton<NotificationService>();
 
 builder.Services.AddHttpClient("MiniPos.Api",
         client => client.BaseAddress = new Uri("http://localhost:5107/"))
     .AddHttpMessageHandler<CookiesHandler>()
-    .AddHttpMessageHandler<UnauthorizedHandler>();
+    .AddHttpMessageHandler<RefreshTokenHandler>();
 
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("MiniPos.Api"));
 
